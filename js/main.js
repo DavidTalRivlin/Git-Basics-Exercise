5c2d5bfba8900b4d4466d934ce1150f5304320ae
DavidTalRivlin <david.dave.tal@gmail.com>
2023-11-15 17:36:26 +0200

    modify ballsize function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
-function onBallClick(){
-    console.log('hi')  
+function onBallClick(elBall) {
+      var ballSize = elBall.offsetHeight
+        
+    elBall.style.height = `${ballSize+50}px`
+    elBall.innerText = `${ballSize+50}`   
 }
